18103c52b70174513ea705de94e05f0d64e89188
chee <chee@snoot.club>
2019-08-14 20:26:03 +0100

    yeet

--- js/background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function () {
-	'use strict';
-	chrome.app.window.create( '../html/index.html', {
-		minWidth: 640,
-		maxWidth: 640,
-		minHeight: 675,
-		maxHeight: 675
-	});
-});
